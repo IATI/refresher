1269a79b9485de448e3b95f9fb8c610b0cb72e5d
Alex Lydiate <alexl@devinit.org>
2021-04-23 08:56:46 +0100

    Reports in the DB and histories retained

--- src/migrations/mig_3.py
+++ src/migrations/mig_3.py
@@ -1,25 +1,26 @@
 upgrade = """
 CREATE TABLE public.validation (
-    id serial PRIMARY KEY,
-    document_id character varying NOT NULL,
-    document_hash character varying NOT NULL,
+    document_hash character varying PRIMARY KEY,
+    document_id character varying NOT NULL,    
     document_url character varying NOT NULL,
     created timestamp without time zone NOT NULL,
     valid boolean NOT NULL,
     report jsonb NOT NULL
 );
 
-ALTER TABLE public.document ADD COLUMN validation integer;
+ALTER TABLE public.document ADD COLUMN validation character varying;
 
 ALTER TABLE public.document DROP COLUMN valid;
 
 ALTER TABLE ONLY public.document
-    ADD CONSTRAINT related_validation FOREIGN KEY (validation) REFERENCES public.validation(id);
+    ADD CONSTRAINT related_validation FOREIGN KEY (validation) REFERENCES public.validation(document_hash);
 """
 
 downgrade = """
     ALTER TABLE public.document
         DROP COLUMN validation;
+
+    ALTER TABLE public.document ADD COLUMN valid boolean;
 
     DROP TABLE public.validation;
 """
