c8ce9a55fee7f17d37769afb8c892e4fac3e5f42
Alex Lydiate <alexl@devinit.org>
2021-05-06 16:16:04 +0100

    Back to 20

--- src/constants/config.py
+++ src/constants/config.py
@@ -14,7 +14,7 @@ config = dict(DATA_SCHEMA = "public",
     DB_NAME = os.getenv('DB_NAME'),
     DDS = dict(
         SKIP_EXISTING_FILES = True,
-        PARALLEL_PROCESSES = 30
+        PARALLEL_PROCESSES = 20
     ),
     VALIDATION = dict(
         PARALLEL_PROCESSES = 1,
