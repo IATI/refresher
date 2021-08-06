0f5d0889571d30850386e6fe49292923b9fb709e
Alex Lydiate <alexl@devinit.org>
2021-08-06 15:58:10 +0100

    adhoc-change

--- src/migrations/mig_9.py
+++ src/migrations/mig_9.py
@@ -1,10 +1,10 @@
 upgrade = """
 CREATE TABLE public.adhoc_validation (
-    id SERIAL,  
-    hash character varying PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    guid character varying,
     filename character varying,
     user_email character varying,
-    created timestamp without time zone NOT NULL,
+    created timestamp without time zone,
     validated timestamp without time zone,
     validation_api_error character varying,
     valid boolean,
