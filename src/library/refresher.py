defdce0f0e15f4400412efa54f976dc808d2580a
akmiller01 <alex.k.miller@gmail.com>
2021-08-24 09:26:02 -0400

    Requests handle 301

--- src/library/refresher.py
+++ src/library/refresher.py
@@ -203,7 +203,21 @@ def download_chunk(chunk, blob_service_client, datasets):
             blob_client.upload_blob_from_url(url, overwrite=True)
             db.updateFileAsDownloaded(conn, id)
         except (AzureExceptions.ResourceNotFoundError) as e:
-            db.updateFileAsDownloadError(conn, id, e.status_code)
+            if '301' in e.reason:
+                try:
+                    download_xml = requests_retry_session(retries=3).get(url=url, timeout=5).content
+                    blob_client.upload_blob(download_xml, overwrite=True)
+                    db.updateFileAsDownloaded(conn, id)
+                except (requests.exceptions.ConnectionError) as e2:
+                    db.updateFileAsDownloadError(conn, id, 0)
+                except (AzureExceptions.ResourceNotFoundError) as e2:
+                    db.updateFileAsDownloadError(conn, id, e2.status_code)
+                except (AzureExceptions.ServiceResponseError) as e2:
+                    logger.warning('Failed to upload XML with url ' + url + ' - Azure error message: ' + e2.message)
+                except Exception as e2:
+                    logger.warning('Failed to upload XML with url ' + url + ' and hash ' + hash)
+            else:
+                db.updateFileAsDownloadError(conn, id, e.status_code)
         except (AzureExceptions.ServiceResponseError) as e:
             logger.warning('Failed to upload XML with url ' + url + ' - Azure error message: ' + e.message)
         except Exception as e:
