47eb38173e4fc04af55bc4553d3bad0ec0d489f3
nosvalds <niko@devinit.org>
2021-09-29 11:36:27 +0100

    use chardet to detect encoding

--- src/library/flatten.py
+++ src/library/flatten.py
@@ -10,24 +10,13 @@ from azure.core import exceptions as AzureExceptions
 import psycopg2
 import library.db as db
 import json
+import library.utils as utils
 
 logger = getLogger()
 
 def chunk_list(l, n):
     for i in range(0, n):
         yield l[i::n]
-
-def get_text_from_blob(downloader):
-    #In order of likelihood
-    charsets = ['UTF-8', 'latin-1', 'UTF-16', 'Windows-1252']
-
-    for charset in charsets:
-        try:
-            return downloader.content_as_text(encoding=charset)
-        except:
-            continue
-
-    raise ValueError('Charset unknown, or not in the list.')
 
 def process_hash_list(document_datasets):
 
@@ -55,7 +44,7 @@ def process_hash_list(document_datasets):
             downloader = blob_client.download_blob()
 
             try:
-                payload = get_text_from_blob(downloader)
+                payload = utils.get_text_from_blob(downloader, file_hash)
             except:
                 logger.warning('Can not identify charset for ' + file_hash + '.xml')
                 continue
---
+++ src/library/utils.py
@@ -0,0 +1,26 @@
+import chardet
+from library.logger import getLogger
+
+logger = getLogger()
+
+def get_text_from_blob(downloader, file_hash):  
+    # save off bytes if we need to detect charset later
+    downloadBytes = downloader.content_as_bytes()
+    try:
+        return downloader.content_as_text()
+    except UnicodeDecodeError:
+        logger.info('File is not UTF-8, trying to detect encoding for file with hash ' + file_hash)
+        pass
+    
+    # If not UTF-8 try to detect charset and decode
+    try:
+        detect_result = chardet.detect(downloadBytes)
+        charset = detect_result['encoding']
+        if charset:
+            logger.info('Charset detected: ' + charset + ' Confidence: ' + str(detect_result['confidence']) + ' Language: ' + detect_result['language'] + ' for file with hash ' + file_hash)
+            return downloader.content_as_text(encoding=charset)
+        logger.warning('No Charset detected for file with hash ' + file_hash + '. Likely a non-text file.')
+        raise
+    except:
+        logger.warning('Could not determine charset to decode for file with hash ' + file_hash)
+        raise
