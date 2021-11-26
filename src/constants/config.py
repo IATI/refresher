34feeb2940acd62d8dba0e36a8cf0af54bee96a4
nosvalds <niko@devinit.org>
2021-11-26 16:13:10 +0000

    reduce solrize parallel processes due to ACI dying

--- src/constants/config.py
+++ src/constants/config.py
@@ -28,7 +28,7 @@ config = dict(DATA_SCHEMA = "public",
         FLATTENER_KEY_VALUE = os.getenv('FLATTENER_KEY_VALUE')
     ),
     SOLRIZE = dict(
-        PARALLEL_PROCESSES = 10,
+        PARALLEL_PROCESSES = 5,
         SOLR_API_URL = os.getenv('SOLR_API_URL'),
         SOLR_USER = os.getenv('SOLR_USER'),
         SOLR_PASSWORD = os.getenv('SOLR_PASSWORD'),
