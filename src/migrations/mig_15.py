dbd56852e0acb6eedcab1172d51f7d4aaec9eb2b
akmiller01 <alex.k.miller@gmail.com>
2021-12-01 14:07:47 -0500

    Create publisher column from join

--- src/migrations/mig_15.py
+++ src/migrations/mig_15.py
@@ -1,5 +1,6 @@
 upgrade = """
 ALTER TABLE public.validation ADD COLUMN publisher character varying;
+UPDATE validation SET publisher = document.publisher FROM document where validation.document_id = document.id;
 """
 downgrade = """
 ALTER TABLE public.validation DROP COLUMN publisher;
