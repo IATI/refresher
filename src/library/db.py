a0eb2685b31321187470264fc93a5108586552d5
nosvalds <niko@devinit.org>
2022-06-21 14:33:40 +0100

    refactor publisher flagging query to make it much faster

--- src/library/db.py
+++ src/library/db.py
@@ -187,15 +187,16 @@ def removeBlackFlag(conn, org_id):
 def blackFlagDubiousPublishers(conn, threshold, period_in_hours):
     cur = conn.cursor()
     sql = """
-    UPDATE publisher as pub
+    UPDATE publisher
     SET black_flag = NOW()
-    WHERE (
-        SELECT COUNT(document_hash)
+    WHERE org_id like (
+        SELECT publisher
         FROM validation 
-        WHERE publisher = pub.org_id
-        AND valid = false
+        WHERE valid = false
         AND NOW() - created < interval ' %(period_in_hours)s hours'
-    ) > %(threshold)s
+        GROUP BY publisher
+        HAVING COUNT(document_hash) >  %(threshold)s
+    )
     """
 
     data = {
