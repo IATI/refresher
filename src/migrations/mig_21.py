d9cb38781b1510f8ea7e2006d15ca3185f22dcc8
nosvalds <niko@devinit.org>
2022-08-16 16:42:59 +0100

    update migrations to trigger re-validation of files with alv2 changes

--- src/migrations/mig_21.py
+++ src/migrations/mig_21.py
@@ -3,7 +3,7 @@ ALTER TABLE public.document ADD COLUMN file_schema_valid BOOLEAN;
 UPDATE public.document
 SET file_schema_valid = public.validation.valid
 FROM public.validation
-WHERE document.validation = public.validation.id
+WHERE document.validation = public.validation.id;
 """
 downgrade = """
 ALTER TABLE public.document
