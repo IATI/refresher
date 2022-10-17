293f831c7e9e2e668655a874774a3cb886d3805f
akmiller01 <alex.k.miller@gmail.com>
2022-10-17 09:26:44 -0400

    Include blank strings for narratives

--- src/library/lakify.py
+++ src/library/lakify.py
@@ -18,8 +18,10 @@ def clean_identifier(identifier):
 
 def recursive_json_nest(element, output):
     element_dict = {'@{}'.format(e_key): element.get(e_key) for e_key in element.keys()}
-    if element.text is not None and element.text.strip()!='':
+    if element.text is not None and element.text.strip() != '':
         element_dict['text()'] = element.text
+    elif element.tag == 'narrative':
+        element_dict['text()'] = ''
     for e_child in element.getchildren():
         element_dict = recursive_json_nest(e_child, element_dict)
     if element.tag in output.keys():
