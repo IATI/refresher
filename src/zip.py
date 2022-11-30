9357a904dc3fced702ac08b63d2ce4fb00837b65
akmiller01 <alex.k.miller@gmail.com>
2022-11-30 12:50:36 -0500

    From Clean

--- src/zip.py
+++ src/zip.py
@@ -14,7 +14,7 @@ logger = getLogger()
 def getPublisherNamesForHashes(conn):
     cur = conn.cursor()
     sql = """
-    SELECT doc.hash, pub.name
+    SELECT doc.id, pub.name
     FROM document AS doc
     LEFT JOIN publisher AS pub ON doc.publisher = pub.org_id
     """
@@ -30,20 +30,21 @@ def main():
     mem_zip = BytesIO()
 
     conn = db.getDirectConnection()
-    all_hashes = getPublisherNamesForHashes(conn)
-    hash_dict = {res[0]: res[1] for res in all_hashes}
+    all_ids = getPublisherNamesForHashes(conn)
+    id_dict = {res[0]: res[1] for res in all_ids}
 
     blob_service_client = BlobServiceClient.from_connection_string(
         config['STORAGE_CONNECTION_STR'])
     container_client = blob_service_client.get_container_client(
-        container=config['ACTIVITIES_LAKE_CONTAINER_NAME'])
+        container=config['CLEAN_CONTAINER_NAME'])
 
     with zipfile.ZipFile(mem_zip, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip:
         for blob in tqdm(container_client.walk_blobs(include=["tags"])):
             if blob.tags:
-                if os.path.splitext(blob.name)[1] == '.xml' and blob.tags['dataset_hash'] in hash_dict.keys():
-                    file_hash = blob.tags['dataset_hash']
-                    publisher_name = hash_dict[file_hash]
+                file_hash, file_ext = os.path.splitext(blob.name)
+                if file_ext == '.xml' and blob.tags['document_id'] in id_dict.keys():
+                    document_id = blob.tags['document_id']
+                    publisher_name = id_dict[document_id]
                     downloader = container_client.download_blob(blob=blob)
                     blob_content = utils.get_text_from_blob(
                         downloader, file_hash)
