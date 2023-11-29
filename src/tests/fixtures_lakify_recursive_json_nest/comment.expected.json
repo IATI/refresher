IATI/refresher

commit a90819b55aa00b64f76e18380fd0845e4bdb220e

Rearrange function to make flow clearer, and only check for inner text upon failing to find text

src/library/lakify.py
@@ -21,26 +21,32 @@ def recursive_json_nest(element, output):
     element_dict = {'@{}'.format(e_key): element.get(e_key)
                     for e_key in element.keys()}
     element_tag = element.tag
-    inner_text = None
-    if element_tag is not etree.Comment and element_tag is not etree.PI:
-        inner_text = ''.join([inner_string.strip()
-                              for inner_string in element.itertext(tag=element_tag)])
-    elif element_tag is etree.Comment:
+    if element_tag is etree.Comment:
         element_tag = 'comment()'
     elif element_tag is etree.PI:
         element_tag = 'PI()'
+
     if element.text is not None and element.text.strip() != '':
         element_dict['text()'] = element.text
-    elif inner_text is not None and inner_text != '':
-        element_dict['text()'] = inner_text
-    elif element_tag == 'narrative':
+    else:
+        inner_text = None
+        if element_tag is not etree.Comment and element_tag is not etree.PI:
+            inner_text = ''.join([inner_string.strip()
+                                  for inner_string in element.itertext(tag=element_tag)])
+        if inner_text is not None and inner_text != '':
+            element_dict['text()'] = inner_text
+
+    if element_tag == 'narrative' and 'text()' not in element_dict:
         element_dict['text()'] = ''
+
     for e_child in element.getchildren():
         element_dict = recursive_json_nest(e_child, element_dict)
+
     if element_tag in output.keys():
         output[element_tag].append(element_dict)
     else:
         output[element_tag] = [element_dict]
+
     return output
 
 

src/tests/fixtures_lakify_recursive_json_nest/edgecase.expected.json
@@ -14,6 +14,9 @@
           "text()": "Test blank attribute"
         },
         {
+          "text()": "Text blank narrative"
+        },
+        {
           "text()": "Test comment"
         },
         {
@@ -44,6 +47,15 @@
         }
       ],
       "text()": "Floating text",
+      "title": [
+        {
+          "narrative": [
+            {
+              "text()": ""
+            }
+          ]
+        }
+      ],
       "transaction": [
         {}
       ],

src/tests/fixtures_lakify_recursive_json_nest/edgecase.input.xml
@@ -4,6 +4,10 @@
         Floating text
         <!--Test blank attribute-->
         <iati-identifier attribute=''>ACT-1</iati-identifier>
+        <!--Text blank narrative-->
+        <title>
+            <narrative></narrative>
+        </title>
         <!--Test comment-->
         <!--Comment-->
         <!--Test processing instruction-->

commit 89c26140f0b08cdcc0d1850bfdf35ca5d29a5691

recursive_json_nest in lakify: Deal with crash seen around comments
Original data had empty comment, so test for that.

Add other comment types to test for those, too.

Crash introduced by a90819b55aa00b64f76e18380fd0845e4bdb220e

src/library/lakify.py
@@ -30,7 +30,7 @@ def recursive_json_nest(element, output):
         element_dict['text()'] = element.text
     else:
         inner_text = None
-        if element_tag is not etree.Comment and element_tag is not etree.PI:
+        if element.tag is not etree.Comment and element.tag is not etree.PI:
             inner_text = ''.join([inner_string.strip()
                                   for inner_string in element.itertext(tag=element_tag)])
         if inner_text is not None and inner_text != '':

src/tests/fixtures_lakify_recursive_json_nest/comment.expected.json
@@ -1,0 +1,54 @@
+{
+  "iati-activity": [
+    {
+      "@last-updated-datetime": "2014-09-10T07:15:37Z",
+      "comment()": [
+        {}
+      ],
+      "iati-identifier": [
+        {
+          "text()": "ACT-1"
+        }
+      ],
+      "reporting-org": [
+        {
+          "@ref": "AA-AAA-123456789",
+          "@secondary-reporter": "0",
+          "@type": "40",
+          "comment()": [
+            {
+              "text()": " No spanish translation was available "
+            }
+          ],
+          "narrative": [
+            {
+              "text()": "Organisation name"
+            },
+            {
+              "@{http://www.w3.org/XML/1998/namespace}lang": "fr",
+              "text()": "Nom de l'organisme"
+            }
+          ]
+        }
+      ],
+      "title": [
+        {
+          "comment()": [
+            {
+              "text()": " <narrative xml:lang=\"es\">T\u00edtulo de la actividad</narrative> "
+            }
+          ],
+          "narrative": [
+            {
+              "text()": "Activity title"
+            },
+            {
+              "@{http://www.w3.org/XML/1998/namespace}lang": "fr",
+              "text()": "Titre de l'activit\u00e9"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}

src/tests/fixtures_lakify_recursive_json_nest/comment.input.xml
@@ -1,0 +1,17 @@
+<iati-activities generated-datetime="2015-10-11T08:15:37Z" version="2.03" linked-data-default="astringhere">
+    <iati-activity last-updated-datetime="2014-09-10T07:15:37Z">
+        <!-- -->
+        <iati-identifier>ACT-1</iati-identifier>
+        <reporting-org ref="AA-AAA-123456789" type="40" secondary-reporter="0">
+            <narrative>Organisation name</narrative>
+            <narrative xml:lang="fr">Nom de l'organisme</narrative>
+            <!-- No spanish translation was available -->
+        </reporting-org>
+
+        <title>
+            <narrative>Activity title</narrative>
+            <narrative xml:lang="fr">Titre de l'activité</narrative>
+            <!-- <narrative xml:lang="es">Título de la actividad</narrative> -->
+        </title>
+    </iati-activity>
+</iati-activities>

src/tests/test_lakify.py
@@ -8,6 +8,7 @@ import pytest
 RECURSIVE_JSON_NEST_FILES = [
     ('basic'),
     ('edgecase'),
+    ('comment'),
 ]
 
 @pytest.mark.parametrize("filename", RECURSIVE_JSON_NEST_FILES)